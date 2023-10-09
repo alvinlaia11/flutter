688c8fa7a6bce83982bae951a57b2dce40103ed5
Alvin Laia <114079285+alvinlaia11@users.noreply.github.com>
2023-10-09 14:41:22 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,82 +1,31 @@
-import 'package:basic/components/htpHelper.dart';
-import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class MyHome extends StatefulWidget {
-  const MyHome({super.key});
+class HttpHelper {
+  final String apiKey;
 
-  @override
-  State<MyHome> createState() => _MyHomeState();
-}
+  HttpHelper(this.apiKey);
 
-class _MyHomeState extends State<MyHome> {
-  late String result;
-  late HttpHelper helper;
-  double temperature = 0.0; // Suhu default, Anda dapat menggantinya sesuai data yang diterima.
+  Future<String> getWeatherData(String cityName) async {
+    final String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+    final Uri uri = Uri.parse('$baseUrl?q=$cityName&appid=$apiKey');
 
-  @override
-  void initState() {
-    super.initState();
-    helper = HttpHelper();
-    result = "";
-    fetchData(); // Panggil method untuk mengambil data cuaca.
-  }
+    final http.Response response = await http.get(uri);
 
-  Future<void> fetchData() async {
-    final weatherData = await helper.getWeatherData("Medan"); // Ganti "Medan" dengan nama kota yang sesuai
-    setState(() {
-      result = weatherData;
-      temperature = parseTemperature(weatherData);
-    });
-  }
-
-  Color getColorBasedOnTemperature(double temperature) {
-    if (temperature < 20) {
-      return Colors.green; // Suhu rendah, warna hijau.
-    } else if (temperature >= 20 && temperature < 30) {
-      return Colors.yellow; // Suhu sedang, warna kuning.
+    if (response.statusCode == 200) {
+      return response.body; // Ini akan berisi data cuaca dalam format JSON.
     } else {
-      return Colors.red; // Suhu tinggi, warna merah.
+      throw Exception('Gagal mengambil data cuaca');
     }
   }
+}
 
-  double parseTemperature(String jsonData) {
-    final data = json.decode(jsonData);
-    final temp = data['main']['temp'];
-    return temp.toDouble();
-  }
+void main() async {
+  String apiKey = "YOUR_API_KEY"; // Ganti dengan kunci API OpenWeather Anda
+  String cityName = "Medan"; // Ganti dengan nama kota yang Anda inginkan
 
-  @override
-  Widget build(BuildContext context) {
-    final temperatureColor = getColorBasedOnTemperature(temperature);
+  HttpHelper helper = HttpHelper(apiKey);
+  String weatherData = await helper.getWeatherData(cityName);
 
-    return Scaffold(
-      appBar: AppBar(title: Text('Weather Data')),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                color: temperatureColor,
-                child: Center(
-                  child: result.isEmpty
-                      ? CircularProgressIndicator()
-                      : Text(
-                          '$temperature °C', // Menampilkan suhu dalam kotak.
-                          style: TextStyle(color: Colors.white),
-                        ),
-                ),
-              ),
-              SizedBox(height: 20), // Spasi antara dua bagian tampilan
-              Text(
-                'Medan', // Menampilkan nama kota.
-                style: TextStyle(fontSize: 24),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  print("Data Cuaca untuk $cityName:\n$weatherData");
 }
