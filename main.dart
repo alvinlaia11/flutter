cabb738e4fbe54060e9f2fa1a20d880d8e938804
Alvin Laia <114079285+alvinlaia11@users.noreply.github.com>
2023-10-09 14:31:45 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,10 @@
 import 'package:http/http.dart' as http;
 
 class HttpHelper {
-  final String _apiKey =
-      "439d4b804bc8187953eb36d2a8c26a02"; // Ganti dengan kunci API Anda
-  final String _baseUrl =
-      "https://openweathermap.org/data/2.5/weather?id=1214520&appid=439d4b804bc8187953eb36d2a8c26a02";
-      
-      
+  Future<String> getWeatherData(String apiKey, String cityId) async {
+    final String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+    final Uri uri = Uri.parse('$baseUrl?id=$cityId&appid=$apiKey');
 
-  Future<String> getWeatherData(String cityName) async {
-    final Uri uri = Uri.parse('$_baseUrl?q=$cityName&appid=$_apiKey');
     final http.Response response = await http.get(uri);
 
     if (response.statusCode == 200) {
@@ -18,4 +13,19 @@ class HttpHelper {
       throw Exception('Failed to fetch weather data');
     }
   }
+}
+
+void main() async {
+  HttpHelper helper = HttpHelper();
+
+  String apiKey1 = "API_KEY_1"; // Ganti dengan kunci API pertama
+  String apiKey2 = "API_KEY_2"; // Ganti dengan kunci API kedua
+  String cityId1 = "CITY_ID_1"; // Ganti dengan ID kota pertama
+  String cityId2 = "CITY_ID_2"; // Ganti dengan ID kota kedua
+
+  String weatherData1 = await helper.getWeatherData(apiKey1, cityId1);
+  String weatherData2 = await helper.getWeatherData(apiKey2, cityId2);
+
+  print("Weather Data for City 1: $weatherData1");
+  print("Weather Data for City 2: $weatherData2");
 }
