a9bb0174c3e286e51eea4e22328d34cf1c2839c3
Alvin Laia <114079285+alvinlaia11@users.noreply.github.com>
2023-10-09 13:37:44 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,110 +1,16 @@
-import 'package:basic/profile/profile_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ProfileScreen()));
-                },
-                child: const Text('Profile')),
-            Image.asset(
-              'assets/img2.png',
-              width: 200,
-            ),
-            Image.asset('assets/img1.png'),
-            const Icon(Icons.check_circle_outline_rounded),
-            const Text.rich(TextSpan(text: 'Ayok ', children: <TextSpan>[
-              TextSpan(
-                  text: 'Belajar ',
-                  style: TextStyle(fontStyle: FontStyle.italic)),
-              TextSpan(
-                  text: 'Menghitung',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-            ])),
-            const Text(
-              'Total:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.blueGrey,
-                letterSpacing: 2,
-              ),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Center(
-              child: Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.yellow,
-                child: Align(
-                  alignment: FractionalOffset(0.2, 0.6),
-                  child: Container(
-                    height: 40.0,
-                    width: 40.0,
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+class HttpHelper {
+  final String _urlKey = "?api_key=e074f9ca65bc00321b22666bc51130c9";
+  final String _urlBase = "https://api.openweathermap.org/data/2.5/weather?";
+  Future<String> getMovie() async {
+    var url = Uri.parse(_urlBase + '/3/movie/now_playing' + _urlKey);
+    http.Response result = await http.get(url);
+    if (result.statusCode == HttpStatus.ok) {
+      String responseBody = result.body;
+      return responseBody;
+    }
+    return result.statusCode.toString();
   }
 }
